6130b73555c463f8b1eefb2a11d1cdfc7d3e6dd4
Mitchell Hentges <mitchhentges@protonmail.com>
2016-02-23 17:53:58 +0100

    Figure out basic data structures for store.c

--- project/src/main.c
+++ project/src/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "store.h"
 #include "test.c"
 
--- project/src/store.c
+++ project/src/store.c
@@ -2,16 +2,29 @@
 // Created by mitch on 2/23/16.
 //
 
+#define MAX_LOCKS 8
+#define MAX_RESOURCES 1024
 #include "store.h"
+
+struct lock {
+    int id;
+    char* names[MAX_NAME_LENGTH];
+};
+
+int itemCount;
+int lockCount;
+
+struct lock locks[MAX_LOCKS];
+struct resource resources[MAX_RESOURCES];
 
 void store_init()
 {
-
+    itemCount = lockCount = 0;
 }
 
 int lock(int id, int nameCount, char* nameList[])
 {
-    return 0;
+
 }
 
 int unlock(int id, int nameCount, char* nameList[])
